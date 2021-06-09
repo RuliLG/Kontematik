cb3ccd2a1177078d3028c711c17e49a7c8af2bcd
Raúl López <hi@raullg.com>
2021-06-09 10:09:02 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,17 @@
         <script src="https://donkey.kontematik.com/script.js" data-site="STOBJFSB" defer></script>
         <!-- / Fathom -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+        <!-- Hotjar Tracking Code for app.kontematik.com -->
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:2444733,hjsv:6};
+                a=o.getElementsByTagName(‘head’)[0];
+                r=o.createElement(‘script’);r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-white">
