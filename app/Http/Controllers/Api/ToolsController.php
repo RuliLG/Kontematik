0e0c145dc054377829bad1b510ace6e786b62208
Raúl López <hi@raullg.com>
2021-08-16 22:27:39 +0100

    add api endpoints for extension

--- app/Http/Controllers/Api/ToolsController.php
+++ app/Http/Controllers/Api/ToolsController.php
@@ -7,6 +7,7 @@ use App\Exceptions\LimitReachedException;
 use App\Exceptions\RateLimitException;
 use App\Exceptions\UnsafePrompt;
 use App\Http\Controllers\Controller;
+use App\Models\SavedResult;
 use App\Models\Service;
 use App\Models\ServiceCategory;
 use App\Services\Copywriter;
@@ -20,8 +21,7 @@ class ToolsController extends Controller
         $serviceIds = Service::enabled()->select('id')->get()->pluck('id');
         $categories = ServiceCategory::with([
                 'services' => function ($query) use ($serviceIds) {
-                    $query->whereIn('id', empty($serviceIds) ? [-1] : $serviceIds)
-                        ->where('is_enabled', true);
+                    $query->whereIn('id', empty($serviceIds) ? [-1] : $serviceIds);
                 }
             ])
             ->with('services.fields')
@@ -35,6 +35,39 @@ class ToolsController extends Controller
         return response()->json([
             'categories' => $categories,
         ]);
+    }
+
+    public function show (Service $tool)
+    {
+        if (!$tool->is_enabled) {
+            return response()->json(['error' => 'Tool not found'], 404);
+        }
+
+        $tool->load('fields');
+
+        return response()->json($tool);
+    }
+
+    public function save (Service $tool, Request $request)
+    {
+        $request->validate([
+            'result' => 'required|string',
+            'result_id' => 'required|exists:results,id',
+            'params' => 'required|array',
+        ]);
+        if (!$tool->is_enabled) {
+            return response()->json(['error' => 'Tool not found'], 404);
+        }
+
+        $saved = new SavedResult;
+        $saved->service_id = $tool->id;
+        $saved->result_id = $request->result_id;
+        $saved->user_id = auth()->id();
+        $saved->params = is_string($request->params) ? $request->params : json_encode($request->params);
+        $saved->output = $request->result;
+        $saved->save();
+
+        return response()->json($tool);
     }
 
     public function inference (Service $tool, Request $request)
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\ToolsController;
 use App\Http\Controllers\OauthController;
 use App\Http\Controllers\UnsplashController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,11 +22,31 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function () {
     Route::post('login', [LoginController::class, 'login']);
 
-    Route::middleware('auth:api', 'subscribed')->group(function () {
+    Route::get('validate/{token}', function ($token) {
+        $user = User::where('api_token', $token)->first();
+        return response()->json([
+            'success' => $user ? true : false,
+        ]);
+    });
+
+    Route::middleware('auth:api')->group(function () {
+        Route::get('hooks', function () {
+            if (auth()->user()->role === 'admin') {
+                $hooks = json_decode(file_get_contents(storage_path('app/hooks.json')), true);
+                return response()->json($hooks);
+            }
+
+            return response()->json(['error' => 'No permissions'], 401);
+        });
         Route::get('tools', [ToolsController::class, 'index']);
-        Route::post('{tool:slug}/inference', [ToolsController::class, 'inference']);
-        Route::post('oauth', [OauthController::class, 'perform']);
-        Route::get('oauth/{tool:slug}/actions', [OauthController::class, 'getActions']);
-        Route::get('unsplash', [UnsplashController::class, 'generate']);
+        Route::get('tools/{tool:slug}', [ToolsController::class, 'show']);
+
+        Route::middleware('subscribed')->group(function () {
+            Route::post('{tool:slug}/inference', [ToolsController::class, 'inference']);
+            Route::post('{tool:slug}/save', [ToolsController::class, 'save']);
+            Route::post('oauth', [OauthController::class, 'perform']);
+            Route::get('oauth/{tool:slug}/actions', [OauthController::class, 'getActions']);
+            Route::get('unsplash', [UnsplashController::class, 'generate']);
+        });
     });
 });
